d3abaaccc6797aa6b7b28b555d0e95445c3cebf6
Professor <Professor@SN-363526.edu.fiesc.com.br>
2023-03-27 21:49:58 -0300

    chore: add node server

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = 3000
+
+const app = express()
+app.use(bodyParser.json())
+
+app.listen(port, () => {
+    console.log('API executando')
+})
