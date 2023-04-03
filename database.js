3aec6b06c441df265cfc2b0807895241431007c5
BRENO MORAIS ALVES <breno_alves@estudante.sesisenai.org.br>
2023-04-03 19:40:28 -0300

    chore: add the database file

---
+++ database.js
@@ -0,0 +1,13 @@
+const pg = require("pg")
+
+const database = new pg.Client("postgres://rxfrhsgn:1n2BoiZQSkbzFF5hhY6r0cvsGsqLqWSh@babar.db.elephantsql.com/rxfrhsgn")
+
+database.connect((erro)=>{
+    if (erro) {
+        return console.log("Não foi possível se conectar com o ElephantSQL" , erro)
+    }else{
+        return console.log("Conectado ao ElephantSQL!")
+    }
+})
+
+module.exports = database
